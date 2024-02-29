d9fac1f72798befcd230a8a6e323a06deea18842
omarrwalid <omarwaleed1907@gmail.com>
2024-02-29 19:29:12 +0200

    added gpa

--- Task_1.py
+++ Task_1.py
@@ -1,11 +1,14 @@
 student_data = {}  
+student_gpa = {}
 
 num_students=int(input("Enter the Number of Students"))
 for x in range(num_students):
         student_name = input("Enter student name: ")
         student_id = input("Enter student ID: ")
+        student_GPA = input("Enter student GPA: ")
         student_data[student_name] = student_id
-        
+        student_gpa[student_name] = student_GPA
+
 print(student_data)        
-        
+print(student_gpa)
 
